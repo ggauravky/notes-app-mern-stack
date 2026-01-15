35e238478798fba2aea44d0317b1152381257ed7
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-01-15 20:16:37 +0530

    Add Upstash rate limiting middleware to server
    Integrated @upstash/ratelimit and @upstash/redis packages. Added configuration and middleware for rate limiting requests using Upstash. Applied the rate limiter to all incoming requests in the Express server.

---
+++ backend/src/middleware/rateLimiter.js
@@ -0,0 +1,19 @@
+import ratelimit from "../config/upstash.js";
+
+const rateLimiter = async (req, res, next) => {
+  try {
+    const { success } = await ratelimit.limit("my-limit-key");
+
+    if (!success) {
+      return res
+        .status(429)
+        .json({ message: "Too many requests, please try again later." });
+    }
+    next();
+  } catch (error) {
+    console.error("Rate Limiter Error:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export default rateLimiter;
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,18 +2,20 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-connectDB();
-
 app.use(express.json());
+app.use(rateLimiter)
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(PORT, () => {
-  console.log("Server is running on port :" + PORT);
+connectDB().then(() => {
+      app.listen(PORT, () => {
+      console.log("Server is running on port :" + PORT);
+  });
 });
