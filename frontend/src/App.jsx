eb3bce98a8c330aede43024924533ef9e89507c5
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-01-19 20:33:55 +0530

    Initialize frontend with React, Vite, and Tailwind
    Set up a new frontend project using React, Vite, and Tailwind CSS. Added initial project structure, configuration files for ESLint, PostCSS, and Tailwind, as well as basic pages and entry points. Included .gitignore and project metadata.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import { Routes, Route } from 'react-router'
+import HomePage from './pages/HomePage.jsx'
+import CreatePage from './pages/CreatePage.jsx'
+import NoteDetailPage from './pages/NoteDetailPage.jsx'
+
+const App = () => {
+  return (
+    <div data-theme="forest">
+      <button className="btn btn-primary">A DaisyUI Button</button>
+      <div className="card card-compact bg-base-100 w-96 shadow-xl">
+  <figure>
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      alt="Shoes" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+      </Routes>
+
+    </div>
+  )
+}
+
+export default App
